cea7bc84359b7b4aa094a93b6afbce6bb7605bd5
Manohar9392 <vmanureddy428@gmail.com>
2025-12-27 16:21:38 +0530

    Assessment Completed

---
+++ Test-1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using model;
+
+
+
+public class Program
+{
+    static PatientBill? LastBill=null;
+    static bool HasLastBill=false;
+    /// <summary>
+    /// Starting Point
+    /// </summary>
+    /// <param name="args"></param>
+    
+    public static void Main(string[] args)
+    {
+        int choice=-1;// variable to store user entered choice from menu
+        bool flag =true;//variable to control the while loop
+
+        #region Menu bar
+        
+        
+        while(flag){
+            //Displaying the menu
+        Console.WriteLine("======================MediSure Clinic Billing================================");
+        Console.WriteLine("1. Create New Bill (Enter patient Details)");
+        Console.WriteLine("2. View Last Bill");
+        Console.WriteLine("3. Clear Last Bill");
+        Console.WriteLine("4. Exit");
+
+        Console.Write("Enter your option: ");
+        choice=int.TryParse(Console.ReadLine(),out choice)?choice:-1;
+        
+
+        switch (choice)
+        {
+            case(1)://Take the New Bill
+            LastBill=new PatientBill();
+            HasLastBill=true;
+            break;
+            case(2):                 //Give the details  of Existed Bill if existed otherwise returns given info
+            if(HasLastBill)
+                {
+                    LastBill.Details();
+                }
+                else
+                {
+                    Console.WriteLine("No bill available.Please create a new bill first");
+                }
+            break;
+            case(3):// Clear the bill
+            LastBill=null;
+            HasLastBill=false;
+            Console.WriteLine("last bill cleared");
+            break;
+            case(4)://it will close the application
+            flag =false;
+            Console.WriteLine("Thank you. Application closed normally.");
+            break;
+            default:
+            Console.WriteLine("Invalid Choice. Try again");
+            break;
+            
+
+
+        }
+        }
+        #endregion
+
+    
+
+
+        
+        
+    }
+}
---
+++ Test-1/model/Class1.cs
@@ -0,0 +1,95 @@
+﻿namespace model
+{
+    public class PatientBill   //class created with name PatientBill to store patient details
+{
+    public string BillId{get;set;}
+    public string PatientName{get;set;}
+    public bool HasInsurance{get;set;}
+
+    public decimal ConsultationFee{get;set;}
+
+    public decimal LabChargess{get;set;}
+    public decimal MedicineCharges{get;set;}
+
+    public decimal GrossAmount{get;set;}
+    public decimal DiscountAmount{get;set;}
+    public decimal FinalPayable{get;set;}
+
+    #region Methods
+
+    /// <summary>
+    /// Constructer to Take New bill details......
+    /// </summary>
+
+    public PatientBill()
+    {
+        Console.Write("Enter Bill Id: ");
+        BillId=Console.ReadLine();
+        Console.Write("Enter patient Name: ");
+        PatientName=Console.ReadLine();
+        Console.Write("Is the patient insured? (true/false):");
+        bool num=bool.TryParse(Console.ReadLine(),out num)?num:false;
+        HasInsurance=num;
+        Console.Write("Enter Consultation Fee: ");
+        decimal num1=decimal.TryParse(Console.ReadLine(),out num1)?num1:0;
+        ConsultationFee=num1;
+        Console.Write("Enter Lab Charges: ");
+        decimal num2=decimal.TryParse(Console.ReadLine(),out num2)?num2:0;
+        LabChargess=num2;
+        Console.Write("Enter Medicine Charges: ");
+        decimal num3=decimal.TryParse(Console.ReadLine(),out  num3)?num3:0;
+        MedicineCharges=num3;
+
+        Console.WriteLine("Bill created successfully.");
+        GrossAmount=ConsultationFee+LabChargess+MedicineCharges;//Calculation of gross from total charges
+        Console.WriteLine($"Gross Amount: {GrossAmount}");
+        if (HasInsurance)
+        {
+            DiscountAmount=GrossAmount*((decimal)0.10); // Giving the  Discount if insurance available
+        }
+        else
+        {
+            DiscountAmount=0;
+        }
+        Console.WriteLine($"Discount Amount: {DiscountAmount}");
+        FinalPayable=GrossAmount-DiscountAmount;                  // Total gross-discount
+        Console.WriteLine($"Final Amount: {FinalPayable}");
+        Console.WriteLine("------------------------------------------------------------------------------");
+        
+    }
+
+    /// <summary>
+    /// Generating The details of Current Bill....
+    /// </summary>
+
+    
+    public void Details()
+    {
+        Console.WriteLine("-------------- Last Bill-------------");
+        Console.WriteLine($"BillId: {BillId}");
+        Console.WriteLine($"patient: {PatientName}");
+        if(HasInsurance)
+        {
+            Console.WriteLine("Insured: Yes");
+        }
+        else
+        {
+            Console.WriteLine("Insured: No");
+        }
+        Console.WriteLine($"Consultation Fee: {ConsultationFee}");
+        Console.WriteLine($"Lab Charges: {LabChargess}");
+        Console.WriteLine($"Medicine Charges: {MedicineCharges}");
+        Console.WriteLine($"Gross Amount: {GrossAmount}");
+        Console.WriteLine($"Discount Amount: {DiscountAmount}");
+        Console.WriteLine($"Final Payable: {FinalPayable}");
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("------------------------------------------------------------------------------");
+    }
+    #endregion
+    
+
+}
+}
+
+
+
